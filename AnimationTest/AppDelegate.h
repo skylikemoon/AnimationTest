b041134040950928e4d5d969282f464f556a6c90
sky <vzwl@sina.cn>
2016-06-04 10:17:52 +0800

    Initial Commit

---
+++ AnimationTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  AnimationTest
+//
+//  Created by eims1 on 16/6/4.
+//  Copyright © 2016年 sky. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
